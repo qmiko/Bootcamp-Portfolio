ed68e6934816323a7a0f01869d32bbd6b56d8c7b
shaquima <senoritabroccolini@gmail.com>
2019-06-11 21:09:02 -0500

    Buttons Working

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#githubLink").click(function(){
+        window.open('https://github.com/qmiko');
+    })
+    $("#linkedIn").click(function(){
+        window.open('https://www.linkedin.com/in/shaquima-johnson-mcswain-b4688b182/');
+    })
+})
